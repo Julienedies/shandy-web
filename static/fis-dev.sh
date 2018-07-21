d83d99b8b3311b0880b11ae81236ef2bdecc5128
gushenwei <haizai@outlook.com>
2018-07-21 13:24:00 +0800

    stage

--- static/fis-dev.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# fis release --file ../fis/dev.js --dest local --watch
-
-fis server start --type node --port 2018 --root ../ && fis release --file ../fis/release.js --dest local --watch
