58d9458be91dbd85306a0ab121cc71302d1614ce
gushenwei <haizai@outlook.com>
2018-11-11 21:26:00 +0800

    update fis-start.sh

--- fis-start.sh
+++ fis-start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#fis server start --type node --port 2018 --root ../ && fis release --file ../fis/dev.js --dest local --watch
+#fis server start --type node --port 2018 --root ../ && fis release --file ../fis/dev.js --dest local --watch -L
 
 cd static/
-fis release --file ../fis/dev.js --dest local --watch -L
+fis release --file ../fis/dev.js --dest local --watch
